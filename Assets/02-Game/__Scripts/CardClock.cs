bfc4d53d5f282b98edeeae8a5358611efa06c89d
GenericCollegeStudent1 <144394893+GenericCollegeStudent1@users.noreply.github.com>
2023-11-14 21:38:10 -0500

    aaaaaaaaaaaaaaaaaa

--- Assets/02-Game/__Scripts/CardClock.cs
+++ Assets/02-Game/__Scripts/CardClock.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-// An enum defines a variable type with a few prenamed values // a
+
 public enum CardState
 {
     drawpile,
@@ -9,25 +9,17 @@ public enum CardState
     target,
     discard
 }
+
 public class CardClock : Card
 {
-
-    [Header("Set Dynamically: CardClock")]
     public CardState state = CardState.drawpile;
-
     public List<CardClock> hiddenBy = new List<CardClock>();
-
     public int layoutID;
-
-
     public SlotDef slotDef;
 
     override public void OnMouseUpAsButton()
     {
-        // Call the CardClicked method on the Prospector singleton
         Clock.S.CardClicked(this);
-
-        // Also call the base class (Card.cs) version of this method
-        base.OnMouseUpAsButton(); // a
+        base.OnMouseUpAsButton();
     }
 }
